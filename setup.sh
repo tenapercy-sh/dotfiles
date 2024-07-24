3aa4f59ec9d6cd1ed77417dd9f7e02791d131b77
tenapercy-sh <163162548+tenapercy-sh@users.noreply.github.com>
2024-07-24 16:47:04 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+cat .zshrc > $HOME/.zshrc
