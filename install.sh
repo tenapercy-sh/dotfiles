e003a56ef2d9514c54a98905f962e252fefa688c
tenapercy-sh <163162548+tenapercy-sh@users.noreply.github.com>
2024-07-24 17:22:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,8 +30,6 @@ ln -sf $DOTFILES/ignore $HOME/.config/git/ignore
 sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-source $ZSH_CUSTOM/plugins/zsh-autosuggestions
 echo "Dotfile setup complete."
