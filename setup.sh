dc707d2ae83d698615dee400c9e40d0ed2543c93
tenapercy-sh <163162548+tenapercy-sh@users.noreply.github.com>
2024-07-24 16:51:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,23 @@ sudo apt update && sudo apt install -y zsh nodejs npm
 
 
 # Symlink various dotfiles to home directory
-export DOTFILES=/workspaces/.codespaces/.persistedshare/dotfiles
-ln -sf $DOTFILES/.zshrc $HOME/.zshrc
-ln -sf $DOTFILES/.bashrc $HOME/.bashrc
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
+
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
+
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
+}
+
+create_symlinks
 
 # Set up global ignore file
 mkdir -p $HOME/.config/git
